109450d008dc9f641eb245f1c54d64f71339c07d
Muhammad Ali Rasheed <alirasheedmd@gmail.com>
2021-08-04 20:46:53 +0500

    JSX

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import "./footer.scss"
+import "./Footer.scss"
 import { Container, Image } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import shipping from "../images/features/shipping.svg"
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import store from "./store"
 import "./index.scss"
-import App from "./App"
+import App from "./App.jsx"
 import * as serviceWorker from "./serviceWorker"
 import "./bootstrap.min.css"
 
