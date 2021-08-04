3d1bf7d6245f059475c6c610db69e9f0c40b8667
Muhammad Ali Rasheed <alirasheedmd@gmail.com>
2021-08-04 13:21:09 +0500

    design footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,50 @@
 import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Form } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
     <div id="footer">
       <footer id="large-screen">
-        <Container>
-          <Row>
-            <Col className="text-center py-3">Copyright &copy; DIMS</Col>
-          </Row>
+        <Container className="body">
+          <div className="flex-display py-3">
+            <div className="flex-display direction-col">
+              <div className="footer-heading">Logo</div>
+              <div>Buy wide range of products from top brands.</div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="flex-display direction-col">
+              <div className="footer-heading">Quick Links</div>
+
+              <div></div>
+              <div></div>
+            </div>
+            <div className="flex-display direction-col">
+              <div className="footer-heading">Contact</div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="flex-display direction-col grow-2">
+              <div className="footer-heading">
+                <p>Subscribe To Our Weekly Newsletter</p>
+              </div>
+              <div>
+                <span>For Latest News and Offers</span>
+              </div>
+              <div>
+                <Form>
+                  <Form.Group>
+                    <Form.Control
+                      type="text"
+                      placeholder="Email Address"
+                      className="news"
+                    />
+                  </Form.Group>
+                </Form>
+              </div>
+            </div>
+          </div>
         </Container>
       </footer>
       <footer id="small-screen">
