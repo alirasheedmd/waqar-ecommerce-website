2d5a0c9b2a173da8ce46e48c05be4a2b4115d761
Muhammad Ali Rasheed <alirasheedmd@gmail.com>
2021-08-12 15:16:31 +0500

    Page Screen Add Body

---
+++ frontend/src/screens/PageEditScreen.jsx
@@ -0,0 +1,180 @@
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { Form, Button } from "react-bootstrap"
+import { useDispatch, useSelector } from "react-redux"
+import FormContainer from "../components/FormContainer"
+import Message from "../components/Message"
+import Loader from "../components/Loader"
+import { listProductDetails, updateProduct } from "../actions/productAction"
+import { PRODUCT_UPDATE_RESET } from "../constants/productContants"
+import axios from "axios"
+
+const ProductEditScreen = ({ match, history }) => {
+  const productId = match.params.id
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+
+  const dispatch = useDispatch()
+
+  const productDetails = useSelector((state) => state.productDetails)
+  const { loading, error, product } = productDetails
+
+  const productUpdate = useSelector((state) => state.productUpdate)
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = productUpdate
+
+  useEffect(() => {
+    if (successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET })
+      history.push("/admin/productlist/")
+    } else {
+      if (!product.name || productId !== product._id) {
+        dispatch(listProductDetails(productId))
+      } else {
+        setName(product.name)
+        setDescription(product.description)
+        setPrice(product.price)
+        setBrand(product.brand)
+        setCategory(product.category)
+        setCountInStock(product.countInStock)
+        setImage(product.image)
+        setIsFeatured(product.isFeatured)
+      }
+    }
+  }, [dispatch, product, productId, history, successUpdate])
+
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0]
+    const formData = new FormData()
+    formData.append("file", file)
+    formData.append("upload_preset", "q4ihwqxa")
+    setUploading(true)
+
+    try {
+      const { data } = await axios.post(
+        "https://api.cloudinary.com/v1_1/magwatt/image/upload",
+        formData
+      )
+      const { secure_url } = data
+      setImage(secure_url)
+      setUploading(false)
+    } catch (error) {
+      console.error(error)
+      setUploading(false)
+    }
+  }
+  const submitHandler = (e) => {
+    e.preventDefault()
+    const updatedProduct = {
+      name,
+      description,
+      price,
+      brand,
+      category,
+      countInStock,
+      image,
+      isFeatured,
+    }
+    dispatch(updateProduct(updatedProduct, productId))
+  }
+
+  return (
+    <>
+      <Link to="/admin/productlist" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Update Product</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlid="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlid="description">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlid="price">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlid="brand">
+              <Form.Label>Brand</Form.Label>
+              <Form.Control
+                type="text"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlid="category">
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type="text"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlid="countInStock">
+              <Form.Label>Stock</Form.Label>
+              <Form.Control
+                type="number"
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlid="image">
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type="text"
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              ></Form.Control>
+              <Form.File
+                id="image-file"
+                label="Choose File"
+                custom
+                onChange={uploadFileHandler}
+              ></Form.File>
+              {uploading && <Loader />}
+            </Form.Group>
+            <Form.Check
+              type="checkbox"
+              id="isFeaturedCheckbox"
+              className="checkbox"
+              label={`Featured`}
+              defaultChecked={isFeatured}
+              onChange={(e) => setIsFeatured(e.target.checked)}
+            />
+            <Button type="submit" variant="primary">
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  )
+}
+
+export default ProductEditScreen
