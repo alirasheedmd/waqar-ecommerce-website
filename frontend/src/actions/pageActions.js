ec2bd4df56407e351b6a54a8cb551c107151c017
Muhammad Ali Rasheed <alirasheedmd@gmail.com>
2021-08-12 16:26:09 +0500

    Edit Page Action

--- frontend/src/actions/pageActions.js
+++ frontend/src/actions/pageActions.js
@@ -3,6 +3,9 @@ import {
   PAGE_DETAIL_FAIL,
   PAGE_DETAIL_REQUEST,
   PAGE_DETAIL_SUCCESS,
+  PAGE_EDIT_FAIL,
+  PAGE_EDIT_REQUEST,
+  PAGE_EDIT_SUCCESS,
   PAGE_LIST_FAIL,
   PAGE_LIST_REQUEST,
   PAGE_LIST_SUCCESS,
@@ -55,6 +58,40 @@ export const getSinglePage = (slug) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PAGE_DETAIL_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const editSinglePage = (id, page) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PAGE_EDIT_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await Axios.post(`/api/pages/${id}`, page, config)
+    data &&
+      dispatch({
+        type: PAGE_EDIT_SUCCESS,
+        payload: data,
+      })
+  } catch (error) {
+    dispatch({
+      type: PAGE_EDIT_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
